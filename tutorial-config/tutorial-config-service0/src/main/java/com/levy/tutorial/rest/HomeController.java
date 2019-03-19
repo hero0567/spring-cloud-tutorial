09d5d282e94f59ce1ac19a45b46ed831ae06fabb
Lin Xia <lin.xia@nuance.com>
2019-03-19 14:48:05 +0800

    add standalone config server

--- tutorial-config/tutorial-config-service0/src/main/java/com/levy/tutorial/rest/HomeController.java
+++ tutorial-config/tutorial-config-service0/src/main/java/com/levy/tutorial/rest/HomeController.java
@@ -2,10 +2,12 @@ package com.levy.tutorial.rest;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HomeController {
 
     @Value("${config.name}")
